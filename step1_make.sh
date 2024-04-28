040c944e93cda59e496009ab55d91180314887d5
fongyk <fx520@mail.ustc.edu.cn>
2024-04-28 22:28:35 +0800

    update

---
+++ step1_make.sh
@@ -0,0 +1,15 @@
+#!/opt/homebrew/bin/bash
+cd /Users/fong/Documents/github_repo/fongyq
+mv blog/docs blog/build
+cd blog
+if [ "$1" = 'clean' ]
+then
+    make clean
+fi
+make html
+rm -r doctrees
+mv build/doctrees .
+mv build/html docs
+rm -r build
+cp custom.css docs/_static
+cd ../
